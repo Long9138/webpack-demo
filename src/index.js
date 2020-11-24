b857d17b450a2d41ad3712e451ad67a01a7c38e1
张允隆 <ron.yl.zhang@teleone.cn>
2020-11-24 18:37:52 +0800

    管理資源篇學習

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './cat.png';
 
 function component() {
-    const element = document.createElement('div');
-  
-    // lodash，现在通过一个 script 引入
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+  const element = document.createElement('div');
+
+  // lodash，现在通过一个 script 引入
+  element.innerHTML = _.join(['Hello', 'webpack', '你好'], ' ');
+  element.classList.add('hello');
+
+  // 将图像添加到我们已经存在的 div 中。
+  const myIcon = new Image();
+  myIcon.src = Icon;
+  myIcon.width = '200'
+
+  element.appendChild(myIcon);
+
+  return element;
+}
+
+document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,37 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+			},
+			{
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+			},
+		],
+	},
+
+
+	// 解決：WARNING in asset size limit: The following asset(s) exceed the recommended size limit (244 KiB)
+	performance: {
+		hints: "warning", // 枚举
+		maxAssetSize: 30000000, // 整数类型（以字节为单位）
+		maxEntrypointSize: 50000000, // 整数类型（以字节为单位）
+		assetFilter: function (assetFilename) {
+			// 提供资源文件名的断言函数
+			return assetFilename.endsWith('.css') || assetFilename.endsWith('.js');
+		}
+	}
 }
